99673cde8f5f37e9d6e7da207d52fdfe2c18b05b
Bryan C Guner <bryan.guner@gmail.com>
2021-12-13 02:21:20 +0000

    hi

---
+++ CONTAINER/react-shopping-cart-ashton-stom/src/App.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import { Route } from 'react-router-dom';
+import data from './data';
+
+// Components
+import Navigation from './components/Navigation';
+import Products from './components/Products';
+import ShoppingCart from './components/ShoppingCart';
+import ProductContext from './contexts/ProductContext';
+import CartContext from './contexts/CartContext';
+
+function App() {
+	const [products] = useState(data);
+	const [cart, setCart] = useState([]);
+
+	const addItem = item => {
+		setCart([...cart, item])
+	};
+
+	return (
+		<div className="App">
+			<ProductContext.Provider value={{ products, addItem }}>
+				<CartContext.Provider value={cart}>
+					<Navigation cart={cart} />
+
+					{/* Routes */}
+					<Route exact path="/">
+						<Products />
+					</Route>
+
+					<Route path="/cart">
+						<ShoppingCart cart={cart} />
+					</Route>
+				</CartContext.Provider>
+			</ProductContext.Provider>
+		</div>
+	);
+}
+
+export default App;
---
+++ CONTAINER/react-shopping-cart-ashton-stom/src/components/Navigation.js
@@ -0,0 +1,17 @@
+import React, { useContext } from 'react';
+import { NavLink } from 'react-router-dom';
+import CartContext from '../contexts/CartContext';
+
+const Navigation = () => {
+	const cart = useContext(CartContext)
+	return (
+		<div className="navigation">
+			<NavLink to="/">Products</NavLink>
+			<NavLink to="/cart">
+				Cart <span>{cart.length}</span>
+			</NavLink>
+		</div>
+	);
+};
+
+export default Navigation;
