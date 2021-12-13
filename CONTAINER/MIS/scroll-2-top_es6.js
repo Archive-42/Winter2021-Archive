fdf7aaf368e7dbb31cfdd5ec1aa1de16de934bbe
Bryan C Guner <bryan.guner@gmail.com>
2021-12-13 02:12:22 +0000

    hi

---
+++ CONTAINER/MIS/scroll-2-top_es6.js
@@ -0,0 +1,24 @@
+const scrollToTopBtn = document.querySelector(".scrollToTopBtn");
+const rootElement = document.documentElement;
+
+function handleScroll() {
+  // Do something on scroll
+  const scrollTotal = rootElement.scrollHeight - rootElement.clientHeight;
+  if (rootElement.scrollTop / scrollTotal > 0.8) {
+    // Show button
+    scrollToTopBtn.classList.add("showBtn");
+  } else {
+    // Hide button
+    scrollToTopBtn.classList.remove("showBtn");
+  }
+}
+
+function scrollToTop() {
+  // Scroll to top logic
+  rootElement.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+}
+scrollToTopBtn.addEventListener("click", scrollToTop);
+document.addEventListener("scroll", handleScroll);
